6876c2ea1d7e9746ebb6da523b0e278845e89ce1
jieqiboh <jieqiboh5836@gmail.com>
2024-09-08 23:19:42 +0800

    More leetcode qns

--- leetcode/114.FlattenBinaryTreeToLinkedList/main.cpp
+++ leetcode/114.FlattenBinaryTreeToLinkedList/main.cpp
@@ -12,11 +12,9 @@
 #include <sstream>
 #include <bitset>
 #include <utility>
+#include <numeric>
 
 using namespace std;
-
-typedef vector<int> vi;
-typedef long long ll;
 
 void fast() {
     ios::sync_with_stdio(false);
@@ -41,43 +39,26 @@ struct TreeNode {
 class Solution {
 public:
     void flatten(TreeNode *root) {
-        dfs(root);
-    }
-    TreeNode* dfs(TreeNode *root) {
-        // flattens this tree into a linked list, and returns ptr to the tail end
         if (root == nullptr) {
-            return nullptr;
-        } else {
-            TreeNode *leftTail = dfs(root->left);
-            TreeNode *rightTail = dfs(root->right);
-            if (leftTail) {
-                leftTail->right = root->right;
-                root->right = root->left;
-                root->left = nullptr;
-                return leftTail;
-            }
-            if (rightTail) return rightTail;
-            return root;
+            return;
+        }
+        flatten(root->left);
+        flatten(root->right);
+
+        TreeNode *left = root->left;
+        TreeNode *right = root->right;
+        if (root->left) {
+            TreeNode *end = left;
+            while (end->right) end = end->right;
+            root->right = root->left;
+            root->left = nullptr;
+            end->right = right;
         }
     }
 };
 
 int main() {
     fast();
-
-    // Construct the tree manually
-    TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(5);
-    root->left->left = new TreeNode(3);
-    root->left->right = new TreeNode(4);
-    root->right->right = new TreeNode(6);
-
-    // Create a Solution object and call the flatten method
-    Solution solution;
-    solution.flatten(root);
-
-    return 0;
 
     return 0;
 }
